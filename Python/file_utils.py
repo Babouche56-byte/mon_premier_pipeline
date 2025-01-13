1c2565d4939d1f414d008770552bcf09621c0e86
user <nalbo.j@stjolorient.fr>
2025-01-13 11:48:07 +0100

    ajout encoding="utf-8"

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -2,10 +2,9 @@
 
 def diff(file_first, file_second):
     """Fonction retournant True si deux fichiers sont différents."""
-    with open(file_first, 'r') as f1, open(file_second, 'r') as f2:
+    with open(file_first, 'r', encoding="utf-8") as f1, open(file_second, 'r', encoding="utf-8") as f2:
         return f1.read() != f2.read()
 
 def same(file_first, file_second):
     """Fonction retournant True si deux fichiers sont identiques."""
-    with open(file_first, 'r') as f1, open(file_second, 'r') as f2:
-        return f1.read() == f2.read()
+    return not diff(file_first, file_second)
